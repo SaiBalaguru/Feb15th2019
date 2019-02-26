5c3e6b0588e96d535bce59628949d0a370784fb1
Sai <gsaibalaguru@gmail.com>
2019-02-26 16:50:19 +0530

    changes

--- Project/Code/Html & JS/ECME6.js
+++ Project/Code/Html & JS/ECME6.js
@@ -25,3 +25,36 @@ class Department extends Student
 let dept = new Department();
 console.log(dept);
 console.log(dept.a);
+let m = new Map();
+m.set("name","Sai");
+m.set("name1","saibg");
+m.forEach((v,k)=>{
+    console.log(v);
+    console.log(k);
+})
+console.log(m.size);
+m.delete("name");// always deleting using the key value. 
+m.forEach((v,k)=>{
+    console.log(v);
+    console.log(k);
+})
+let s = new Set();
+s.add("Sai");
+s.add("Bala");
+s.add("Guru");
+console.log(s);
+s.forEach((v)=>{
+console.log(v);
+})
+console.log("New Property");
+var user = {
+    name: "asd",
+    age: 14,
+    Gender: "Male"
+}
+var {name,age} = {...user};
+console.log(name);
+console.log(age);
+var [a,b] = [1, 2] ;
+console.log(a);
+console.log(b);
